e2bc8e7d0f0fa7e66f312de33c0da388c1c7056f
0xRooted <vibheks@gmail.com>
2022-07-19 11:20:48 -0700

    Updated

---
+++ static/mem.js
@@ -0,0 +1,11 @@
+function loginPopup() {
+    document.getElementById("thisniggerhere").style.display = "block";
+}
+
+function popup_login() {
+    alert('Please Login First !');
+}
+
+function popu_main() {
+    alert('Nigga');
+}
